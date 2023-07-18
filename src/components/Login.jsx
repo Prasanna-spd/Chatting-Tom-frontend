bb7d82c229af5cd52dfc7b9301ded751f6600bbf
Sarada Prasanna Dash <dashsharada@gmail.com>
2023-07-18 23:30:46 +0530

    created all elements of login and signup

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -8,14 +8,79 @@ import {
   VStack,
 } from "@chakra-ui/react";
 import React, { useState } from "react";
+import { useToast } from "@chakra-ui/react";
+import axios from "axios";
+import { BASE_URL } from "../helper/allUrls";
 
-function Signup() {
+function Login() {
+  const toast = useToast();
+
   const [email, setEmail] = useState();
   const [password, setPassword] = useState();
   const [show, setShow] = useState();
+  const [loading, setLoading] = useState(false);
 
-  const submitHandler = () => {};
-  const handleClick = () => {};
+  const submitHandler = async (e) => {
+    e.preventDefault();
+    setLoading(true);
+    if (!email || !password) {
+      toast({
+        title: "Please Fill all the Feilds",
+        status: "warning",
+        duration: 5000,
+        isClosable: true,
+        position: "top",
+      });
+      setLoading(false);
+      return;
+    }
+    console.log(email, password);
+    try {
+      const config = {
+        headers: {
+          "Content-type": "application/json",
+        },
+      };
+
+      const response = await axios.post(
+        `${BASE_URL}/api/users/login`,
+        {
+          email,
+          password,
+        },
+
+        {
+          withCredentials: true,
+        },
+        config
+      );
+
+      console.log(response);
+      toast({
+        title: "Logged IN!!",
+        description: response.data.message,
+        status: "success",
+        duration: 5000,
+        isClosable: true,
+        position: "top",
+      });
+      localStorage.setItem("userInfo", JSON.stringify(response.data));
+      setLoading(false);
+    } catch (error) {
+      toast({
+        title: "Error Occured!",
+        description: error.response.data.message,
+        status: "error",
+        duration: 5000,
+        isClosable: true,
+        position: "top",
+      });
+      setLoading(false);
+    }
+  };
+  const handleClick = () => {
+    setShow(!show);
+  };
 
   return (
     <VStack spacing={2}>
@@ -70,4 +135,4 @@ function Signup() {
   );
 }
 
-export default Signup;
+export default Login;
--- src/components/Sign-up.jsx
+++ src/components/Sign-up.jsx
@@ -1,5 +1,4 @@
 import {
-  Box,
   Button,
   FormControl,
   FormLabel,
@@ -8,19 +7,95 @@ import {
   InputRightElement,
   VStack,
 } from "@chakra-ui/react";
+import { useToast } from "@chakra-ui/react";
+import axios from "axios";
 import React, { useState } from "react";
+import { BASE_URL } from "../helper/allUrls";
 
 function Signup() {
+  const toast = useToast();
   const [name, setName] = useState();
   const [email, setEmail] = useState();
   const [confirmpassword, setConfirmpassword] = useState();
   const [password, setPassword] = useState();
-  const [pic, setPic] = useState();
+  const [loading, setLoading] = useState(false);
+  // const [pic, setPic] = useState();
   const [show, setShow] = useState();
 
-  const submitHandler = () => {};
-  const handleClick = () => {};
-  const postDetails = () => {};
+  const submitHandler = async (e) => {
+    e.preventDefault();
+    setLoading(true);
+    if (!name || !email || !password || !confirmpassword) {
+      toast({
+        title: "Please Fill all the Feilds",
+        status: "warning",
+        duration: 5000,
+        isClosable: true,
+        position: "top",
+      });
+      setLoading(false);
+      return;
+    }
+
+    if (password !== confirmpassword) {
+      toast({
+        title: "Passwords Do Not Match",
+        status: "warning",
+        duration: 5000,
+        isClosable: true,
+        position: "top",
+      });
+      return;
+    }
+    console.log(name, email, password);
+    try {
+      const config = {
+        headers: {
+          "Content-type": "application/json",
+        },
+      };
+
+      const response = await axios.post(
+        `${BASE_URL}/api/users/new`,
+        {
+          name,
+          email,
+          password,
+        },
+
+        {
+          withCredentials: true,
+        },
+        config
+      );
+
+      console.log(response);
+      toast({
+        title: "Account Created !!",
+        description: response.data.message,
+        status: "success",
+        duration: 5000,
+        isClosable: true,
+        position: "top",
+      });
+      localStorage.setItem("userInfo", JSON.stringify(response.data));
+      setLoading(false);
+    } catch (error) {
+      toast({
+        title: "Error Occured!",
+        description: error.response.data.message,
+        status: "error",
+        duration: 5000,
+        isClosable: true,
+        position: "top",
+      });
+      setLoading(false);
+    }
+  };
+  const handleClick = () => {
+    setShow(!show);
+  };
+  // const postDetails = () => {};
 
   return (
     <VStack spacing={2}>
@@ -73,7 +148,7 @@ function Signup() {
           </InputRightElement>
         </InputGroup>
       </FormControl>
-      <FormControl id="pic">
+      {/* <FormControl id="pic">
         <FormLabel fontSize="sm">Upload your Picture</FormLabel>
         <Input
           size="sm"
@@ -81,7 +156,7 @@ function Signup() {
           accept="image/*"
           onChange={(e) => postDetails(e.target.files[0])}
         />
-      </FormControl>
+      </FormControl> */}
       <Button
         colorScheme="blue"
         size="sm"
